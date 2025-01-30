2b8bfd76c7491c1b29775fbb8bd661f3a96ac3e7
randomguy2356 <trocheniefajnie@gmail.com>
2025-01-30 19:03:08 +0100

    house that if u press f turns into wireframe

--- main.cpp
+++ main.cpp
@@ -10,9 +10,27 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
 
 float vertices[] = {
-	-0.5f, -0.5f, 0.0f,
-	 0.5f, -0.5f, 0.0f,
-	 0.0f,  0.5f, 0.0f
+	 0.5f,  0.5f, 0.0f, //top right
+	 0.5f, -0.5f, 0.0f, //bottom right
+	-0.5f,  0.5f, 0.0f, //top left
+	-0.5f, -0.5f, 0.0f, //bottom left
+	 0.0f,  0.9f, 0.0f  //house roof top
+};
+
+unsigned int indices[] = {
+	0, 1, 2,//first triangle
+	2, 3, 1,//second triangle
+	0, 2, 4	//roof
+};
+
+unsigned int indicesWF[] = {
+	0, 1,
+	0, 2,
+	1, 2,
+	2, 3,
+	1, 3,
+	0, 4,
+	2, 4
 };
 
 const char *vertexShaderSource = "#version 330 core\n"
@@ -26,7 +44,7 @@ const char *fragmentShaderSource = "#version 330 core\n"
 	"out vec4 FragColor;\n"
 	"\n"
 	"void main(){\n"
-	"FragColor = vec4(0.0f, 0.8f, 0.8f, 0.5f);\n"
+	"FragColor = vec4(0.0f, 0.5f, 0.5f, 0.0f);\n"
 	"}\n";
 unsigned int fragmentShader;
 
@@ -34,6 +52,11 @@ unsigned int shaderProgram;
 
 unsigned int VBO;
 unsigned int VAO;
+unsigned int EBO;
+unsigned int EBOWF;
+
+bool wireframe = false;
+
 int main(){
 	if(!glfwInit()){
 		std::cout << "failed to initialise GLFW";
@@ -68,12 +91,21 @@ int main(){
 	
 	shaderProgram = glCreateProgram();
 	
+	glGenBuffers(1, &EBO);
+	glGenBuffers(1, &EBOWF);
 	glGenBuffers(1, &VBO);
 	glGenVertexArrays(1, &VAO);
-	
+	//VAO
 	glBindVertexArray(VAO);
+	//VBO
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	//EBO
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	//EBOWF
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBOWF);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indicesWF), indicesWF, GL_STATIC_DRAW);
 	
 	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
 	glCompileShader(vertexShader);
@@ -90,20 +122,24 @@ int main(){
 	
 	while(!glfwWindowShouldClose(window)){
 		processInput(window);
-	
+		
 		glClearColor(0.2f, 0.2f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
-	
 		glUseProgram(shaderProgram);
-		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
-	
+		if(!wireframe){		
+			glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+			glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
+		}
+		else{
+			glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBOWF);
+			glDrawElements(GL_LINES, 14, GL_UNSIGNED_INT, 0);	
+		}
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 	glDeleteShader(fragmentShader);
 	glDeleteShader(vertexShader);
-
+	
 	glfwTerminate();
 	return 0;
 }
@@ -113,8 +149,16 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height){
 	glViewport(0, 0, width, height);
 }
 
+bool toggle = true;
 void processInput(GLFWwindow *window){
 	if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS){
 		glfwSetWindowShouldClose(window, true);
 	}
+	if(glfwGetKey(window, GLFW_KEY_F) == GLFW_PRESS){
+		if(toggle == true){
+			wireframe = !wireframe;
+			toggle = false;
+		}
+	}
+	else{toggle = true;}
 }
